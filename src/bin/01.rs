da691439d52dc51710020884ec6dd820107971df
xinj10 <xinj10@outlook.com>
2024-02-06 14:10:27 +0800

    feat: day1 part2 done

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,7 +3,6 @@ use regex::Regex;
 use std::collections::HashMap;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    //TODO: use regex
     let lines: Vec<&str> = input.lines().collect();
     let mut sum: u32 = 0;
     let re = Regex::new(r"\d").unwrap();
@@ -21,8 +20,8 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    let _number_table: HashMap<&str, u32> = HashMap::from([
+pub fn part_two(input: &str) -> Option<u32> {
+    let number_table: HashMap<&str, u32> = HashMap::from([
         ("one", 1),
         ("two", 2),
         ("three", 3),
@@ -33,7 +32,29 @@ pub fn part_two(_input: &str) -> Option<u32> {
         ("eight", 8),
         ("nine", 9),
     ]);
-    None
+    let re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").expect("invalid regex");
+    let mut sum = 0;
+    let lines: Vec<&str> = input.lines().collect();
+    for hay in lines {
+        let mut start = 0;
+        let mut numbers: Vec<u32> = Vec::new();
+        loop {
+            let om = re.find_at(hay, start);
+            match om {
+                Some(m) => {
+                    start = m.start() + 1;
+                    let number = m.as_str();
+                    numbers.push(match number_table.get(number) {
+                        Some(num_int) => *num_int,
+                        None => number.parse().expect("invalid number"),
+                    });
+                }
+                None => break,
+            }
+        }
+        sum += numbers[0] * 10 + numbers[numbers.len() - 1];
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -43,7 +64,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(142));
+        assert_eq!(result, Some(209));
     }
 
     #[test]
